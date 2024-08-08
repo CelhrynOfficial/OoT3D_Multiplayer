a0e21ec6cd5930e58fb8dda6ca070356cd143426
Celhryn <159892385+CelhrynOfficial@users.noreply.github.com>
2024-08-08 16:41:16 +0200

    Update multiplayer.cpp

--- source/settings/multiplayer.cpp
+++ source/settings/multiplayer.cpp
@@ -3,14 +3,12 @@
 namespace MultiplayerMenu {
 
 Option MP_SharedProgress = Option::Bool("Shared Progress", {"Off", "On"},         {mp_SharedProgressDesc});
-Option MP_SyncId         = Option::U8  (2, "Sync ID",      {Settings::NumOpts(1, 1)},       {mp_SyncIdDesc}, OptionCategory::Cosmetic);
 Option MP_SharedHealth   = Option::Bool("Shared Health",   {"Off", "On"},         {mp_SharedHealthDesc});
 Option MP_SharedRupees   = Option::Bool("Shared Rupees",   {"Off", "On"},         {mp_SharedRupeesDesc});
 Option MP_SharedAmmo     = Option::Bool("Shared Ammo",     {"Off", "On"},         {mp_SharedAmmoDesc});
 
 std::vector<Option*> multiplayerOptions = {
     &MP_SharedProgress,
-    &MP_SyncId,
     &MP_SharedHealth,
     &MP_SharedRupees,
     &MP_SharedAmmo,
